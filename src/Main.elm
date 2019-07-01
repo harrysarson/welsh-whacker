60e86de4ecb2d11d21e0409276d5742654bf16dc
Harry Sarson <harry.sarson@hotmail.co.uk>
2019-07-01 15:04:21 +0100

    Get elm format out of an infinite loop

--- src/Main.elm
+++ src/Main.elm
@@ -4,7 +4,7 @@ module Main exposing (main)
 
   - welsh data
   - <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-  - welsh lion: [[[https://upload.wikimedia.org/wikipedia/commons/1/11/Welsh\_dragon.svg](https://upload.wikimedia.org/wikipedia/commons/1/11/Welsh_dragon.svg)](https://upload.wikimedia.org/wikipedia/commons/1/11/Welsh_dragon.svg)](https://upload.wikimedia.org/wikipedia/commons/1/11/Welsh_dragon.svg)
+  - welsh lion: <https://upload.wikimedia.org/wikipedia/commons/1/11/>
 
 -}
 
